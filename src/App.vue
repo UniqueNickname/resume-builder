4d88e7a41beb293d8d9484826fdea69d8aa99b88
UniqueNickname <dr.art-47@yandex.ru>
2022-03-03 19:17:26 +0600

    save 6

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
 <template>
-  <div id="core" class="base-container mx-auto bg-white grid grid-cols-4 gap-5 text-lg">
+  <div id="core" class="grid grid-cols-4 gap-5 mx-auto text-lg bg-white base-container">
     <div class="col-span-4 h-[0px]">
-      <div class="text-center block h-8 mt-4">
-        <h1 class="uppercase font-bold text-2xl">Кочура Иван Александрович</h1>
+      <div class="block h-8 mt-4 text-center">
+        <h1 class="text-2xl font-bold uppercase">Кочура Иван Александрович</h1>
       </div>
       <div class="flex items-center justify-center mx-auto">
-        <div class="w-64 text-center flex items-center justify-between">
+        <div class="flex items-center justify-between w-64 text-center">
           <p class="text-opacity-50">Front-end разработчик</p>
           <MapMarker />
           <p class="text-opacity-50">Омск</p>
         </div>
       </div>
     </div>
-    <div class="col-span-1 text-center space-y-10 -mt-32">
+    <div class="col-span-1 mt-0 space-y-10 text-center">
       <div class="flex items-center justify-center h-[360px] pr-2 pl-4">
         <img :src="photo" alt="">
       </div>
       <left-block title="Контакты" :options="contacts" />
       <left-block title="Навыки" :options="skills" />
     </div>
-    <div class="col-span-3 space-y-4 -mt-28">
+    <div class="col-span-3 mt-20 space-y-4">
       <div class="">
         <div class="flex items-center justify-start space-x-4">
           <account />
-          <h2 class="uppercase font-bold">Личная информация</h2>
+          <h2 class="font-bold uppercase">Личная информация</h2>
         </div>
         <div class="border-l-2 border-gray-500 pl-6 ml-[7px] mt-2">
           <named-list :options="personal" />
@@ -32,13 +32,38 @@
       <div class="">
         <div class="flex items-center justify-start space-x-4">
           <briefcase />
-          <h2 class="uppercase font-bold">Опыт работы</h2>
+          <h2 class="font-bold uppercase">Опыт работы</h2>
         </div>
         <div class="border-l-2 border-gray-500 pl-6 ml-[7px] mt-2 space-y-4">
           <div>
-            <h3 class="font-bold text-xl">Front-end разработчик</h3>
-            <p class="text-opacity-50 text-black">Спички</p>
-            <small class="text-opacity-50 text-black">март 2021 - ноябрь 2021</small>
+            <h3 class="text-xl font-bold">Front-end разработчик</h3>
+            <p class="text-black text-opacity-50">WineStyle</p>
+            <small class="text-black text-opacity-50">февраль 2022 - март 2022</small>
+            <div class="pl-4 space-y-4">
+              <p class="pr-20">
+                Компания WineStyle занимается продажей элитных вин и прочих товаров.
+                Мы командой разрабатывали новый интернет магазин с нуля.
+                Проект на Nuxt 3 с postcss.
+              </p>
+              <work-list
+                title="В основном разрабатывал библиотеку компонентов в соответствии с Atomic Design"
+                :options="tasksWineStyle"
+              />
+              <!-- <p>
+                Для каждого фильтра генерируется отдельная страница (e.g. /wine/red/france)
+              </p>
+              <p>
+                Реализовал функционал для рендеринга в HTML при запросе данных для фильтров на стороне сервера всех ссылок для всех фильтров, чтоб скормить их поисковым ботам.
+              </p>
+              <p>
+                При этом на стороне клиента произойдёт рендер только 5 ссылок в каждой категории. Остальные доступны только после нажатия на "Показать все".
+              </p> -->
+            </div>
+          </div>
+          <div>
+            <h3 class="text-xl font-bold">Front-end разработчик</h3>
+            <p class="text-black text-opacity-50">Спички</p>
+            <small class="text-black text-opacity-50">март 2021 - ноябрь 2021</small>
             <div class="pl-4 space-y-4">
               <p class="pr-20">
                 Спички - стартап по генерации озвучки для статей. Работал
@@ -52,9 +77,9 @@
             </div>
           </div>
           <div>
-            <h3 class="font-bold text-xl">Front-end разработчик</h3>
-            <p class="text-opacity-50 text-black">Кварк</p>
-            <small class="text-opacity-50 text-black">июнь 2020 - март 2021</small>
+            <h3 class="text-xl font-bold">Front-end разработчик</h3>
+            <p class="text-black text-opacity-50">Кварк</p>
+            <small class="text-black text-opacity-50">июнь 2020 - март 2021</small>
             <div class="pl-4 space-y-4">
               <p class="pr-24">
                 Кварк - аутсорс компания. В основном работал на проекте medzona.com.
@@ -66,7 +91,7 @@
       </div>
       <div class="flex items-center justify-start space-x-4">
         <school />
-        <h2 class="uppercase font-bold">Образование</h2>
+        <h2 class="font-bold uppercase">Образование</h2>
       </div>
       <div class="border-l-2 border-gray-500 pl-6 ml-[7px] mt-2 space-y-4">
         <h3 class="font-bold">Омский государственный университет</h3>
@@ -74,7 +99,7 @@
       </div>
       <div class="flex items-center justify-start space-x-4">
         <information />
-        <h2 class="uppercase font-bold">Дополнительная информация</h2>
+        <h2 class="font-bold uppercase">Дополнительная информация</h2>
       </div>
       <div class="border-l-2 border-gray-500 pl-6 ml-[7px] mt-2 space-y-4">
         <named-list :options="about" />
@@ -97,7 +122,8 @@ import photo from './assets/img/photo2.png'
 const contacts = [
   '+79136745378',
   'dr.art-47@yandex.ru',
-  'https://github.com/ UniqueNickname/'
+  'https://github.com/ UniqueNickname/',
+  'Telegram - @uniquenickname13',
 ]
 
 const skills = [
@@ -116,6 +142,12 @@ const personal = {
   'Образование': 'Высшее неполное',
   'Дата рождения': '1 августа 1999',
 }
+
+const tasksWineStyle = [
+  'Множество простых компонентов',
+  'Карусель. Компонент в slot принимает любое количество элементов, но внутри отрендерится только определённое количество, указанное в props. При пролистывании элементов вправо рендерятся отсутствующие элементы.',
+  'Карточка товара. Компонент бывает в двух вариантах и двух размерах. При переключении вида (grid, list) или изменении размера экрана не меняется HTML. При этом элементы карточки меняются местами. Реализовано с помощью css grid.'
+]
 
 const tasksSpeechki = [
   'интерфейс админ панели',
@@ -141,7 +173,7 @@ const about = {
   'Занятия в свободное время': 'Фитнес, видеоигры',
   'Личные качества': [
     'Отсутствие вредных привычек',
-    'Самостоятельность - т.к. весь front-end на мне, многие решения принимались лично мной',
+    'Самостоятельность - т.к. в компании Спички весь front-end на мне, многие решения принимались лично мной',
     'Инициативность - предлагал множество решений по улучшению UI и UX',
     'Быстрая обучаемость - часто приходилось переключаться между технологиями. Уже поработал и с vue 3, писал плагин для webpack с поддержкой v4 и v5, быстро изучил svelte, писал библиотеки для npm, плагины для vue и react'
   ],
